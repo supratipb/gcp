24b1c333c5bc1b19b614d754bd4a1745181436bb
unknown <Supratip Banerjee>
2020-07-14 09:49:08 +0530

    dialogflow

---
+++ push_code_to_git.bat
@@ -0,0 +1,13 @@
+::change location of your new repo
+git init
+git add .
+git commit -m "dialogflow"
+git config --global user.email supratip.banerjee@yahoo.in
+git config --global user.email "Supratip Banerjee"
+
+::create the below <repo_name> in git
+::name a new repo for local the below <repo_name> in git
+::git remote add <repo> https://github.com/supratipb/<repo_name>.git
+
+git remote add dialogflow https://github.com/supratipb/gcp.git
+git push dialogflow master  
